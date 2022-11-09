b0cd24279d407fbe9b156069919c95ae78d3e387
Tom McGlynn <tom@mcglynns.org>
2022-11-09 13:42:39 -0500

    Version 1.4

---
+++ js/DisplayParam.js
@@ -0,0 +1,22 @@
+// Normally ID will identify a span or div element
+// whose content is to specified by passing a key/value pair to the page.
+// Intended for HTML pages with minimal JavaScript
+function DisplayParam(id, key, dft) {
+    let elem = document.getElementById(id);
+    if (elem == null) {
+        return;
+    }
+    let params = new URLSearchParams(document.location.search);
+    let val = params.get(key);
+    let nval = Number(val);
+    if (!isNaN(nval)) {        
+//                        Epoch        Day0/2000   Day0/Unix 
+        const millis = (nval / 86400 + 2451544.5 - 2440587.5) * 86400000;
+        const dateLocal = new Date(millis).toISOString();                       
+        elem.innerHTML =  dateLocal.substring(0,dateLocal.length-14);
+        
+    } else {
+         elem.innerHTML = val;
+    }
+}
+
